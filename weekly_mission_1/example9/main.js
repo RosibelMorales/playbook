ffc72f3f67c1b8b35bd70e28a8f4c134bca80c5b
Rosibel Morales <rossibel8217@gmail.com>
2022-04-13 17:23:35 -0500

    Actualizando ejercicio 9

--- weekly_mission_1/example9/main.js
+++ weekly_mission_1/example9/main.js
@@ -1,4 +1,23 @@
-//Ejercicio 
+/* Ejercicio 
+   Crea el archivo pokemon.js. 3, Crea una clase Pokemon, exportala usando CommonJS, de tal forma que pueda usarse como se indica previamente.
+        Crea la clase.
+        Crea un constructor que permita recibir un nombre.
+        Crea la función sayHello().
+        Crea la función sayMessage().
+        Exporta la clase para que pueda ser importada en el archivo main.js.
+
+        $ node main.js
+
+        output
+        Mi pokemon pikachu te saluda!!!
+        Mi pokemon pikachu dice: Heey!
+        Mi pokemon bulbasaur te saluda!!!
+        Mi pokemon bulbasaur dice: Heey!
+        Mi pokemon charmander te saluda!!!
+        Mi pokemon charmander dice: Heey!
+        Mi pokemon squirtle te saluda!!!
+        Mi pokemon squirtle dice: Heey!
+**/
 
 const Pokemon = require('./pokemon')
 
